03b961e4ff262ebc4a4677380f0711e4eb266cd1
ozcode2009 <84883214+ozcode2009@users.noreply.github.com>
2026-08-23 21:15:06 -0400

    Change peerPrefix from 'MSH_' to 'MH'

--- ios/Classes/BlePeripheralHandler.swift
+++ ios/Classes/BlePeripheralHandler.swift
@@ -24,7 +24,7 @@ final class BlePeripheralHandler: NSObject {
         string: "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
     )
 
-    private static let peerPrefix = "MSH_"
+    private static let peerPrefix = "MH"
 
     // MARK: - Properties
 
